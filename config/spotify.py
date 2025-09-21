650622da69d2027d6c92f475ce3bea5c853226c9
AnzFarhan <farhanddaini@gmail.com>
2025-09-21 23:34:17 +0800

    Spotify API configuration: Required scopes for the different API features

--- config/spotify.py
+++ config/spotify.py
@@ -11,9 +11,18 @@ class SpotifyConfig:
     """Basic Spotify API configuration"""
     
     def __init__(self):
-        self.client_id = os.getenv('4cf1ea44e49e4ab48c806f3fd6938b74')
-        self.client_secret = os.getenv('e5f24a4570e84274826ea27f3bfeb890')
-        self.redirect_uri = 'https://datapotipy.com/callback'
+        self.client_id = os.getenv('SPOTIFY_CLIENT_ID')
+        self.client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+        self.redirect_uri = os.getenv('SPOTIFY_REDIRECT_URI', 'https://datapotipy.com/callback')
+        
+        # Required scopes for the different API features
+        self.scopes = ' '.join([
+            'user-read-recently-played',    # For getting recently played tracks
+            'user-read-private',            # For getting user details
+            'playlist-read-private',        # For reading playlists
+            'user-library-read',            # For reading saved tracks
+            'user-top-read'                 # For reading top tracks
+        ])
     
     def test_config(self):
         """Test if Spotify configuration is complete"""
